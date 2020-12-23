208f10a24bb07fe346ce57efa0d01f0d8788c482
Tres Tarwater <trest03@gmail.com>
2020-12-23 19:36:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,19 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  @@count = 
+  @@count = 0
+  @@artists = 0 
+  @@genres = 0 
   
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    
   end
  
+ def
  
  end
+ 
+ 
