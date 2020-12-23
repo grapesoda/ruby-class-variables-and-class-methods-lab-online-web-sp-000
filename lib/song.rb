d0d7ffab8c782416876c6f21717c5aac1af96b8c
Tres Tarwater <trest03@gmail.com>
2020-12-23 20:03:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,19 +16,27 @@ class Song
     @@count += 1
   end
  
-  def self.count
-    @@count
-  end
-  
   def self.artists
-    @@artists
+    @@artists.uniq
   end
-  
+
   def self.genres
-    @@genres
+    @@genres.uniq
   end
-  
- 
+
+  def self.genre_count
+    @@genres.each do |genre|
+      @@genre_count[genre] = @@genres.count(genre)
+    end
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      @@artist_count[artist] = @@artists.count(artist)
+    end
+    @@artist_count
+  end
  
  end
  
